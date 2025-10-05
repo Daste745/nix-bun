359e2634743d2820c0e6cf5f1f6fa31a1e9e76f2
Daste <stefankar1000@gmail.com>
2025-10-05 19:32:43 +0000

    Add all supported systems

--- flake.nix
+++ flake.nix
@@ -6,7 +6,12 @@
   outputs =
     { nixpkgs, ... }:
     let
-      systems = [ "aarch64-darwin" ];
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
       eachSystem = f: nixpkgs.lib.genAttrs systems (system: f (import nixpkgs { inherit system; }));
     in
     {
